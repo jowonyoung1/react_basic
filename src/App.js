e7fd9288fe9aaa7731149afdc6b951f32973361d
jowonyoung1 <jwy7254@naver.com>
2024-04-23 12:43:02 +0900

    docs: 컴포넌트 스타일링

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import Articles from "./pages/Articles";
-import Article from "./pages/Article";
-import Layout from "./components/Layout";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import MyPage from "./pages/MyPage";
+import StyledBoxDiv from "./components/StyledComponent";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
-      </Route>
-
-      <Route path="/articles" element={<Articles />}>
-        <Route index element={<Navigate replace to="1" />} />
-        <Route path=":id" element={<Article />} />
-      </Route>
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/mypage" element={<MyPage />} />
-
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <StyledBoxDiv />
+    </div>
   );
 }
 
